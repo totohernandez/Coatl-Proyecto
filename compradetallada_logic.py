2c3ebc95e38e463174e9724582beeab58a8ee71b
alexia-avelar <72109860+alexia-avelar@users.noreply.github.com>
2020-11-26 15:54:10 -0600

    Add files via upload

---
+++ compradetallada_logic.py
@@ -0,0 +1,54 @@
+from dx_logic import Logic
+from compradetallada_obj import CompraDetObj
+
+
+class CompraDetLogic(Logic):
+    def __init__(self):
+        super().__init__()
+        self.tableName = "compradetallada"
+
+    def getAllCompras(self):
+        compraList = super().getAllRows(self.tableName)
+        compraObjList = []
+        for element in compraList:
+            newCompra = self.createCompraDetObj(element)
+            compraObjList.append(newCompra)
+        return compraObjList
+
+    # polimorfismo
+    def createCompraDetObj(
+        self, idcompra, tipo, precios_unitarios, cantidad, monto_total
+    ):
+        compraObj = CompraDetObj(id, tipo, precios_unitarios, cantidad, monto_total)
+        return compraObj
+
+    def createCompraObj(self, userDict):
+        compraObj = CompraDetObj(
+            userDict["user"],
+            userDict["password"],
+            userDict["email"],
+            userDict["id"],
+        )
+        return compraObj
+
+    def insertCompra(self, tipo, precios_unitarios, cantidad, monto_total):
+        database = self.database
+        sql = (
+            f"INSERT INTO `cine`.`compradetallada`(`idcompra`,`tipo`,`precios_unitarios`,`cantidad`,`monto_total`) "
+            + f"VALUES(0,'{tipo}','{precios_unitarios}','{cantidad}','{monto_total}');"
+        )
+        rows = database.executeNonQueryRows(sql)
+        return rows
+
+    def updateCompraById(self, id, tipo, precios_unitarios, cantidad, monto_total):
+        database = self.database
+        sql = (
+            "UPDATE `cine`.`compradetallada` "
+            + f"SET `compradetallada` = '{tipo}','{precios_unitarios}','{cantidad}','{monto_total}' "
+            + f"WHERE `idcompra` = {id};"
+        )
+        rows = database.executeNonQueryRows(sql)
+        return rows
+
+    def deleteCompraById(self, idcompra):
+        super().deleteRowById(id, self.tableName)
---
+++ compradetallada_obj.py
@@ -0,0 +1,7 @@
+class CompraDetObj:
+    def __init__(self, tipo, precios_unitarios, cantidad, monto_total, idcompra=0):
+        self.tipo = tipo
+        self.precios_unitarios = precios_unitarios
+        self.cantidad = cantidad
+        self.monto_total = monto_total
+        self.idcompra = idcompra
