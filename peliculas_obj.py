b39b503ac2eda2073c4f6039277ba3269be4592d
Jose Ricardo Hernandez <joserhernandez96@gmail.com>
2020-11-27 00:30:43 -0600

    Add files via upload
    Tablas Peliculas y Sucursal

---
+++ peliculas_obj.py
@@ -0,0 +1,7 @@
+class PeliculasObj:
+    def __init__(self, titulo, descricpion, fecha, hora, id=0):
+        self.id = id
+        self.titulo = titulo
+        self.descripcion = descripcion
+        self.fecha = fecha
+        self.hora = hora
