f17b509ca13206cfec724150723c99acd9c036bf
alexia-avelar <72109860+alexia-avelar@users.noreply.github.com>
2020-11-27 21:12:16 -0600

    Revert "https://github.com/totohernandez/Coatl-Proyecto/pull/1#issue-528421912"

--- sucursal_obj.py
+++
@@ -1,9 +0,0 @@
-class SucursalObj:
-    def __init__(self, nombre, departamento, direccion, id=0):
-        self.id = id
-        self.nombre = nombre
-        self.departamento = departamento
-        self.direccion = direccion
-        self.usuario_idusuario = usuario_idusuario 
-
-
